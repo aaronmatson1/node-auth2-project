46f825e228725ee917dbf7dfe8ce7c69a0b29ea2
aaronmatson1 <matsonaaron1@gmail.com>
2020-06-09 21:24:15 -0600

    Created Index file

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const express = require("express");
+
+const server = express();
+const port = process.env.PORT || 5000;
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send(`<h1>Welcome</h1>`);
+});
+
+server.listen(port, () => {
+  console.log(`Server has been initialized at http://localhost:${port}....`);
+});
